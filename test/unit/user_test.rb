e5247e9dfbf9e87276e146844490d2a74431648c
Fernando Campos <elefante@gmail.com>
2009-05-28 08:04:38 -0300

    Tests working till 129

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,37 +4,12 @@ require 'user'
 class UserTest < ActiveSupport::TestCase
 
   def test_required
-=begin
-    u = User.new
-    u.save
-    assert u.errors[:name]
-    assert u.errors[:login]
-    assert u.errors[:password]
-    assert u.errors[:email]
-=end
     assert_required_values(User, :login => 'estenome', :password => 'estenome', :email => 'estenome@domain.com')
   end
 
   def test_length
-    # login e passwords muito pequenos
-    u = User.new(:login => "a", :password => "a", :email => "test@mail.com", :name => "Emanuel")
-    assert !u.save
-    assert u.errors[:login]
-    assert u.errors[:password]
-
-    # login e passwords muito grandes
-    u.login = "a" * 101
-    u.password = "a" * 55
-    assert !u.save
-    assert u.errors[:login]
-    assert u.errors[:password]
-
-    # login e passwords tamanho OK
-    u.login = "a" * 90
-    u.password = "a" * 45
-    assert u.save
-    assert !u.errors[:login]
-    assert !u.errors[:password]
+    assert_valid_sizes User, {:login => "a", :password => "a"}, {:login => "a" * 90,
+        :password => "a" * 90}, {:login => "a" * 45, :password => "a" * 45}
   end
 
   def test_email_format
@@ -50,14 +25,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_uniqueness
-    u = User.new(:login => "user1", :password => "asdas", :email => "user1@domain1.com", :name => "qualquer nome")
-    assert !u.save
-    assert u.errors[:login]
-    assert u.errors[:email]
-    u.login = "2user1"
-    u.email = "2user1@domain1.com"
-    assert u.save
-    assert !u.errors[:login]
-    assert !u.errors[:email]
+    assert_unique_values User, {:login => "user1", :password => "user1", :email => "user1@domain1.com"},
+        {:login => "fernando", :password => "macaco", :email => "fernando@domain.com"}, {:name, :password, :email}
   end
 end
